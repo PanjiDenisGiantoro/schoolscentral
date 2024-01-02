e245eeeae236ccc750a2064296b2736931f32d8c
PanjiDenisGiantoro <panjidenisgiantoroo@gmail.com>
2024-01-02 18:51:45 +0700

    Initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function ($user) {
+            static::created(function($user){
+                $userTenant = Tenant::create([
+                    'id' => $user->domain
+                ]);
+                $userTenant->domains()->create([
+                    'domain' => $user->domain. '.'. env('APP_CENTRAL_DOMAIN')
+                ]);
+            });
+        });
+    }
+}
